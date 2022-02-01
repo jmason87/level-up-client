129b96658e29b083fc6dbfe78834bf998d4d3198
jmason87 <joewebdev87@gmail.com>
2022-01-31 19:04:27 -0600

    added useHistory import

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 import "./NavBar.css"
 
 export const NavBar = () => {
