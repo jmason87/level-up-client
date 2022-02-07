b383c522530eb774bb3e3da6a9ab0886722104a8
jmason87 <joewebdev87@gmail.com>
2022-02-07 15:19:07 -0600

    through chapter 14

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useHistory, Link } from "react-router-dom/cjs/react-router-dom.min"
-import { deleteEvent, getEvents } from "./EventManager.js"
+import { joinEvent, deleteEvent, getEvents, leaveEvent } from "./EventManager.js"
 
 export const EventList = (props) => {
     const [ events, setEvents ] = useState([])
@@ -24,6 +24,12 @@ export const EventList = (props) => {
                         <div className="event__title">{event.description}</div>
                         <div className="event__players">{event.date} players needed</div>
                         <div className="event__skillLevel">Skill level is {event.time}</div>
+                        {
+                            event.joined ?
+                            <button onClick={() => {leaveEvent(event.id).then(setEvents)}}>Leave Event</button>
+                            :
+                            <button onClick={() => {joinEvent(event.id).then(setEvents)}}>Join Event</button>
+                        }
                         <Link to={`/events/update/${event.id}`}><button>Update</button></Link>
                         <button onClick={() => {deleteEvent(event.id).then(setEvents)}}>Delete</button>
                     </section>
--- src/components/event/EventManager.js
+++ src/components/event/EventManager.js
@@ -1,6 +1,6 @@
 export const getEvents = () => {
     return fetch("http://localhost:8000/events", {
-        headers:{
+        headers: {
             "Authorization": `Token ${localStorage.getItem("lu_token")}`
         }
     })
@@ -10,7 +10,7 @@ export const getEvents = () => {
 export const createEvent = (game) => {
     return fetch("http://localhost:8000/events", {
         method: "POST",
-        headers:{
+        headers: {
             "Authorization": `Token ${localStorage.getItem("lu_token")}`,
             "content-Type": "application/json"
         },
@@ -21,7 +21,7 @@ export const createEvent = (game) => {
 
 export const getSingleEvent = (id) => {
     return fetch(`http://localhost:8000/events/${id}`, {
-        headers:{
+        headers: {
             "Authorization": `Token ${localStorage.getItem("lu_token")}`
         }
     })
@@ -38,15 +38,39 @@ export const updateEvent = (event, id) => {
         },
         body: JSON.stringify(event)
     })
-        
+
 }
 
 export const deleteEvent = (id) => {
     return fetch(`http://localhost:8000/events/${id}`, {
         method: "DELETE",
-        headers:{
+        headers: {
             "Authorization": `Token ${localStorage.getItem("lu_token")}`
         }
     })
-        .then(getEvents)     
+        .then(getEvents)
+}
+
+export const leaveEvent = id => {
+    // TODO: Write the DELETE fetch request to leave an event
+    return fetch(`http://localhost:8000/events/${id}/leave`, {
+        method: "DELETE",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(getEvents)
+}
+
+export const joinEvent = id => {
+    // TODO: Write the POST fetch request to join and event
+    return fetch(`http://localhost:8000/events/${id}/signup`, {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        },
+        body: JSON.stringify(id)
+    })
+        .then(getEvents)
 }
